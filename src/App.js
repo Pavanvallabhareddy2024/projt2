2395beb9ee38574b3f1e10d0fd0b2b36b5b53f9e
Pavan Vallabhareddy <saipavan2024@gmail.com>
2024-11-14 10:20:01 +0530

    Third change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Super creatin a repo and adding to Github</p>
+        <p>Super creating a repo and adding to Github</p>
         <a
           className="App-link"
           href="https://reactjs.org"
